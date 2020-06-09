e045d8c89e56429f64f928e5972bd874bcc6d436
Jovan <jovanfoster9@gmail.com>
2020-06-09 10:23:12 -0400

    fixed sorting algorithm

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,16 @@
 def bubble_sort(arr)
-    arr.each_with_index.map{ |element, index|
-        if element > arr[index + 1]
-            swap = element
-            element = arr[index + 1]
-            arr[index + 1] = swap
+    sorted = false
+    while !sorted do
+        swapped = false
+        for index in (0..arr.length - 2) do
+            if arr[index] > arr[index + 1]
+                arr[index], arr[index + 1] = arr[index + 1], arr[index]
+                swapped = true
+            end
         end
-    }
-end        
-        
+        sorted = !swapped
+    end
+    return arr
+end   
+
+p bubble_sort([6, 7, 3, 15, 25, 1, 6, 3, 5])
